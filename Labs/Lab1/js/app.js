8580466845ceb07aec98cb71452392fa6eaae922
Hedge100 <p.ryan.butler@gmail.com>
2022-01-20 15:30:28 -0500

    wrote code to make a circle follow the cursor and grow when clicked capping at 50.

--- Labs/Lab1/js/app.js
+++ Labs/Lab1/js/app.js
@@ -1,9 +1,18 @@
+//seting up the canvas and printing the state of variable size
 function setup() {
     createCanvas(800, 600);
-    background(40);
+    console.log(size)
 }
-
+//creating the size variable which says how large the circle will be
+var size = 5;
+//sends the value of size to the console then makes a circle follow the cursor with size 5 and growing by one pixel for each tick while the mouse button is held capping at 50.
 function draw() {
-    
-    circle(mouseX, mouseY, frameCount/10);
+    background(255,255,255)
+    console.log(size)
+    if (size<50){
+        if (mouseIsPressed){
+            size = size + 1
+        };
+    }
+    circle(mouseX, mouseY, size);
 }
