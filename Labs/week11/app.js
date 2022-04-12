212bc4934ab710655a7f5ea7f7ee7c9dea4b8fc7
Hedge100 <p.ryan.butler@gmail.com>
2022-04-12 19:23:58 -0400

    fixed tic tac toe checker

--- Labs/week11/app.js
+++ Labs/week11/app.js
@@ -1,28 +1,43 @@
 let grid = [
-    [1,1,2]
-    [0,2,1]
-    [2,0,1]
+    [1,1,0],
+    [1,2,1],
+    [1,0,1]
 ]
 let filteredGrid = []
 let players = ['none','X','O']
+let winnerFound = false
 for(i=0;i<2;i++){
+    //horizonatal win check
     for(x=0;x<3;x++){
         filteredGrid = []
-        filteredGrid[0] = grid[x].filter(square == i+1)
+        filteredGrid[0] = grid[x].filter(function(square){return square == i+1})
         if (filteredGrid[0].length == 3){
             console.log(players[i+1]+ ' Wins!')
+            winnerFound = true
             break
         }
+        //vertical win check
         filteredGrid = []
         for(y=0;y<3;y++){
-            filteredGrid[0] = grid[y][x].filter(square==i+1)
+            filteredGrid[0] = grid.filter(function(row){return row[x]==i+1})
+            console.log(filteredGrid[0])
             if (filteredGrid[0].length == 3){
                 console.log(players[i+1]+ ' Wins!')
+                winnerFound = true
                 break
-            }else if (i==1){
-                console.log ('Nobody Won')
             }
+            
         }
+        if(winnerFound){
+            break
+        }
+    }
+    if(winnerFound){
+        break
     }
 }
 
+
+if (!winnerFound){
+    console.log("Nobody Won!")
+}
